6c0b9b2483e80262129cc0a9e8e71f8758eff087
andrea-livornese <andrealivornese@gmail.com>
2024-01-25 17:01:11 +0100

    BONUS:aggiunto paragrafo sopra il form e aggiunte righe per disabilitare il bottone e gli input del form. fine bonus

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -7,5 +7,9 @@ function passGenerate(){
     let pass = fullname+surname+color+"23784";
     
     document.getElementById("passGenerate").innerHTML+=pass;
-    
+    document.getElementById("button").disabled=true;
+    document.getElementById("nome").disabled=true;
+    document.getElementById("cognome").disabled=true;
+    document.getElementById("colore").disabled=true;
+
 }
