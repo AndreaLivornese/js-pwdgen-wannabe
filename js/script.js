07e31137fe95a8a119562fa201bb784e45fd7245
andrea-livornese <andrealivornese@gmail.com>
2024-01-25 16:37:11 +0100

    creazione script

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
 
-function generatePass(){
-let fullname = document.getElementById("nome").innerHTML;
-let surname = document.getElementById("cognome").innerHTML;
-let color = document.getElementById("colore").innerHTML;
 
+    let fullname = prompt("inserisci il tuo nome");
+    let surname = prompt("inserisci il tuo cognome");
+    let color = prompt("inserisci il tuo colore preferito");
 
-let pass =  fullname+surname+color;
-
-document.getElementById("passGenerate").innerHTML = pass;
-
-}
+    let pass = fullname+surname+color+"23784";
+    
+    document.getElementById("passGenerate").innerHTML+=pass;
+    
